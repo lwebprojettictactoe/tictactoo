2c2b079cd1a6799d699b852055c5912a57fedff2
Adrien Lambersens <adrien062bis@gmail.com>
2017-10-26 12:20:42 +0200

    feat(js-ajouter-partie) : Ajout affichage formulaire création partie

--- web/bundles/tttfirst/js/index.js
+++ web/bundles/tttfirst/js/index.js
@@ -1,6 +1,13 @@
+/*Initialisation des différents élements JQuery*/
 $("#tous").click(function () { typeGame("Toutes"); }); // The value of the selected option
 $("#attente").click(function () { typeGame("En attente"); }); // The value of the selected option
 $("#cours").click(function () { typeGame("En cours"); }); // The value of the selected option
+$("#dropdown-type-game").dropdown();
+
+$(document).ready(function() {
+	$('select').material_select();
+});
+$('select').material_select('destroy');
 
 function typeGame(type) {
 	if(document.getElementById("value-type")){
@@ -13,8 +20,17 @@ function typeGame(type) {
 	}
 }
 
-function loadNodeJS(){
 
+function showForm(){
+	if(!$("#form-ajout").hasClass("scale-out")){
+		document.getElementById("add-partie").innerHTML = "<i class=material-icons>add</i>";
+		$("#form-ajout").addClass("scale-out");
+	}
+	else{
+		document.getElementById("add-partie").innerHTML = "<i class=material-icons>remove</i>";
+
+		$("#form-ajout").removeClass("scale-out");
+	}
 }
 
 function init() {
