7dc50420806ee4f7a9cf10c9e0815939198d8d9c
Adrien Lambersens <adrien062bis@gmail.com>
2017-11-08 22:07:57 +0100

    feat(bdd-js) : Possibilité de trier les parties et d'en créer

--- serveur.js
+++ serveur.js
@@ -12,7 +12,44 @@ var server = http.createServer(function (req, res) {
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
 
-// Quand un client se connecte, on le note dans la console
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('projectGame', 'adrien', 'Mecaplast062SQL', {
+	host: 'localhost',
+	dialect: 'mysql',
+	define: {
+		timestamps: false
+	},
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 10000
+	},
+});
+const Op = Sequelize.Op;
+
+const Parties = sequelize.define('parties', {
+	jeu: {
+		type: Sequelize.STRING
+	},
+	id_utilisateur1:{
+		type: Sequelize.INTEGER
+	},
+	id_utilisateur2:{
+		type: Sequelize.INTEGER
+	},
+	status: {
+		type: Sequelize.STRING
+	},
+	date: {
+		type: Sequelize.DATE,
+		defaultValue: Sequelize.NOW
+	},
+	nom: {
+		type: Sequelize.STRING
+	}
+});
+
+
 io.sockets.on('connection', function (socket) {
 	console.log('Un client est connecté !');
 
@@ -21,14 +58,57 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('new-game', function (newgame) {
-		for(var champs in newgame){
-			console.log(newgame[champs]);
+		for(let champs in newgame){
+			if(newgame[champs] === ""){
+				socket.emit('error-empty-field', champs);
+				return;
+			}
+			console.log(newgame[champs] + " " + champs);
 		}
-		socket.emit('after-create-game', "Tu peux jouer");
-	})
+
+		// Table created
+		Parties.create({
+				nom: newgame["nom"],
+				id_utilisateur1: newgame['id_utilisateur1'],
+				jeu: newgame['jeu'],
+				status: 'En attente',
+				id_utilisateur2: null
+		});
+
+		//socket.emit('after-create-game');
+	});
+	
+	socket.on('fetch-game', function (type) {
+		if(typeof type === 'undefined'){
+			type = 'Toutes;'
+		}
+		if(type === "Toutes"){
+			Parties.findAll({
+				where: {
+					[Op.or] : [{Status: 'En attente'}, {Status: 'En cours'}]
+				},
+				raw: true,
+			}).then(function(parties){socket.emit('a-game', parties);});
+		}
+		else{
+			Parties.findAll({
+				where: {
+					Status: type
+				},
+				raw: true,
+			}).then(function(parties){socket.emit('a-game', parties);});
+		}
+	});
 });
 
-// Quand un client se connecte, on le note dans la console
-
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.');
+	})
+	.catch(err => {
+		console.error('Unable to connect to the database:', err);
+	});
 
 server.listen(8081);
+
