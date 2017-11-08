e3c72d63bbf629a1e7e2c2542d1630e01eff7ec6
Adrien Lambersens <adrien062bis@gmail.com>
2017-11-08 22:14:44 +0100

    feat(js-client) : Ajout des interactions entre le client et le serveur
    Récupération des parties
    Affichage des parties
    Controle si une partie peut être créer

--- web/bundles/tttfirst/js/socketClient.js
+++ web/bundles/tttfirst/js/socketClient.js
@@ -1,15 +1,45 @@
 var socket = io.connect('http://localhost:8081');
 
+function recuperationPartie(type){
+	addIndeter()
+
+	socket.emit("fetch-game", type);
+}
+
 $("#form-add-game").click(function () {
+	addIndeter();
 	socket.emit('new-game', {
-			joueur1: document.getElementById("form-nom-personne").value,
+			id_utilisateur1: document.getElementById("form-nom-personne").value,
 			nom: document.getElementById("form-name").value,
 			jeu: document.getElementById("form-jeu").value
 		}
 	);
+	removeIndeter();
 });
 
 socket.on("after-create-game", function (aftercreategame) {
-	alert(aftercreategame);
 	$("#index-partie").hide();
+});
+
+socket.on("error-empty-field", function (champ) {
+	swal("Veuillez remplir le champ " + champ);
+});
+
+socket.on("a-game", function (parties) {
+	$("#listGame").text('');
+
+	for(let partie in parties){
+
+		$("#listGame").append(
+			"<tr>" +
+				"<td>"+ parties[partie].id + "</td>" +
+				"<td>"+ parties[partie].nom + "</td>" +
+				"<td>"+ parties[partie].jeu + "</td>" +
+				"<td>"+ parties[partie].status + "</td>" +
+				"<td>"+ parties[partie].id_utilisateur1 + "</td>" +
+				"<td>"+ (parties[partie].id_utilisateur2 === null ? '...' : parties[partie].id_utilisateur2) + "</td>" +
+			"</tr>"
+		);
+	}
+	removeIndeter();
 });
