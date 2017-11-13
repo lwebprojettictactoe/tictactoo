5ea3b47f7036bb17d200cba49250217a8a069fd5
Unknown <antoine.lefevre.isn@gmail.com>
2017-11-13 15:35:52 +0100

    Ajout getters/setters entités et Ajout de la ligne stat lors de la création de l'utilisateur

--- src/TTT/FirstBundle/Entity/Partie.php
+++ src/TTT/FirstBundle/Entity/Partie.php
@@ -83,4 +83,94 @@ class Partie
     {
         parent::__construct();
     }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getJeu() {
+        return $this->jeu;
+    }
+
+    function getId_utilisateur1() {
+        return $this->id_utilisateur1;
+    }
+
+    function getId_utilisateur2() {
+        return $this->id_utilisateur2;
+    }
+
+    function getStatus() {
+        return $this->status;
+    }
+
+    function getNom_utilisateur1() {
+        return $this->nom_utilisateur1;
+    }
+
+    function getNom_utilisateur2() {
+        return $this->nom_utilisateur2;
+    }
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function getScore_Joueur1() {
+        return $this->score_Joueur1;
+    }
+
+    function getScore_Joueur2() {
+        return $this->score_Joueur2;
+    }
+
+    function getDate() {
+        return $this->date;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setJeu($jeu) {
+        $this->jeu = $jeu;
+    }
+
+    function setId_utilisateur1(User $id_utilisateur1) {
+        $this->id_utilisateur1 = $id_utilisateur1;
+    }
+
+    function setId_utilisateur2(User $id_utilisateur2) {
+        $this->id_utilisateur2 = $id_utilisateur2;
+    }
+
+    function setStatus($status) {
+        $this->status = $status;
+    }
+
+    function setNom_utilisateur1($nom_utilisateur1) {
+        $this->nom_utilisateur1 = $nom_utilisateur1;
+    }
+
+    function setNom_utilisateur2($nom_utilisateur2) {
+        $this->nom_utilisateur2 = $nom_utilisateur2;
+    }
+
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function setScore_Joueur1($score_Joueur1) {
+        $this->score_Joueur1 = $score_Joueur1;
+    }
+
+    function setScore_Joueur2($score_Joueur2) {
+        $this->score_Joueur2 = $score_Joueur2;
+    }
+
+    function setDate(\DateTime $date) {
+        $this->date = $date;
+    }
+
+
 }
